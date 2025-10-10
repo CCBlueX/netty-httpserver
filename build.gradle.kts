14bf1cf9d5ee4414a83b230b7b7ac10c332e0eb5
Izuna Seikatsu <izuna.seikatsu@ccbluex.net>
2025-10-10 23:59:56 +0200

    release: 2.4.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ val authorName = "ccbluex"
 val projectUrl = "https://github.com/ccbluex/netty-httpserver"
 
 group = "net.ccbluex"
-version = "2.4.0"
+version = "2.4.1"
 
 repositories {
     mavenCentral()
